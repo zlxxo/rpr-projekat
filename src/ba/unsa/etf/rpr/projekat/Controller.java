47e443f79e30a5469728b6bb08eeb4ea7fd28adf
zlxxo <mramic2@etf.unsa.ba>
2019-09-06 15:12:42 +0200

    postavio početni prozor za prijavu na profil

--- src/ba/unsa/etf/rpr/projekat/Controller.java
+++ src/ba/unsa/etf/rpr/projekat/Controller.java
@@ -1,4 +1,32 @@
 package ba.unsa.etf.rpr.projekat;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    public ImageView logoIV;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            File file = new File("resources/images/lo.png");
+            Image image = new Image(file.toURI().toString());
+            logoIV.setImage(image);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void logInDoctor(ActionEvent actionEvent) {
+    }
+
+    public void logInBoss(ActionEvent actionEvent) {
+    }
 }
--- src/ba/unsa/etf/rpr/projekat/Main.java
+++ src/ba/unsa/etf/rpr/projekat/Main.java
@@ -6,13 +6,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import static javafx.scene.layout.Region.USE_PREF_SIZE;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
+        primaryStage.setTitle("Ljekarska ordinacija");
+        primaryStage.setScene(new Scene(root, USE_PREF_SIZE, USE_PREF_SIZE));
         primaryStage.show();
     }
 
