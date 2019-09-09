a8f77815b42d5e2dd6970a79a430b5abac31d072
zlxxo <mramic2@etf.unsa.ba>
2019-09-09 15:39:55 +0200

    dodao podklase za Doctor

--- src/ba/unsa/etf/rpr/projekat/BossLogInController.java
+++ src/ba/unsa/etf/rpr/projekat/BossLogInController.java
@@ -1,7 +1,15 @@
 package ba.unsa.etf.rpr.projekat;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.util.concurrent.TimeUnit;
+
+import static javafx.scene.layout.Region.USE_PREF_SIZE;
 
 public class BossLogInController {
 
@@ -24,6 +32,20 @@ public class BossLogInController {
             if(boss.correctPassword(bossPasswordTF.getText())) {
                 bossPasswordTF.getStyleClass().removeAll("textFieldIncorrect");
                 bossPasswordTF.getStyleClass().add("textFieldCorrect");
+
+                Stage stage = new Stage();
+                Parent root = null;
+                try {
+                    root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
+                    stage.setTitle("Pomoć");
+                    stage.setScene(new Scene(root, USE_PREF_SIZE, USE_PREF_SIZE));
+                    stage.setResizable(false);
+                    stage.show();
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+                Stage stage1 = (Stage) bossUsernameTF.getScene().getWindow();
+                stage1.close();
             } else {
                 bossPasswordTF.getStyleClass().removeAll("textFieldCorrect");
                 bossPasswordTF.getStyleClass().add("textFieldIncorrect");
--- src/ba/unsa/etf/rpr/projekat/DoctorLogInController.java
+++ src/ba/unsa/etf/rpr/projekat/DoctorLogInController.java
@@ -1,7 +1,15 @@
 package ba.unsa.etf.rpr.projekat;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.util.concurrent.TimeUnit;
+
+import static javafx.scene.layout.Region.USE_PREF_SIZE;
 
 public class DoctorLogInController {
 
@@ -23,6 +31,20 @@ public class DoctorLogInController {
             if(doctor.correctPassword(doctorPasswordTF.getText())) {
                 doctorPasswordTF.getStyleClass().removeAll("textFieldIncorrect");
                 doctorPasswordTF.getStyleClass().add("textFieldCorrect");
+
+                Stage stage = new Stage();
+                Parent root = null;
+                try {
+                    root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
+                    stage.setTitle("Pomoć");
+                    stage.setScene(new Scene(root, USE_PREF_SIZE, USE_PREF_SIZE));
+                    stage.setResizable(false);
+                    stage.show();
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+                Stage stage1 = (Stage) doctorUsernameTF.getScene().getWindow();
+                stage1.close();
             } else {
                 doctorPasswordTF.getStyleClass().removeAll("textFieldCorrect");
                 doctorPasswordTF.getStyleClass().add("textFieldIncorrect");
